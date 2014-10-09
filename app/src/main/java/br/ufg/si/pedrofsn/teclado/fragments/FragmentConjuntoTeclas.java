572d6dab1e2bfb72c8b3e74f0602f55a5689ebc2
pedrofsn <pedrokra@gmail.com>
2014-10-09 10:29:17 -0300

    Correções de layout e refactoring

--- app/src/main/java/br/ufg/si/pedrofsn/teclado/fragments/FragmentConjuntoTeclas.java
+++ app/src/main/java/br/ufg/si/pedrofsn/teclado/fragments/FragmentConjuntoTeclas.java
@@ -19,13 +19,13 @@ public class FragmentConjuntoTeclas extends Fragment {
 
     private GridView gridView;
     private AdapterTecla customGridAdapter;
-    private List<Visografema> gridArray;
+    private List<Visografema> gridTeclas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.adapter_conjunto_teclas, container, false);
 
-        gridArray = new ArrayList<Visografema>();
+        gridTeclas = new ArrayList<Visografema>();
 
         Bundle args = getArguments();
 
@@ -34,7 +34,7 @@ public class FragmentConjuntoTeclas extends Fragment {
         gridView = (GridView) rootView.findViewById(R.id.gridViewTeclas);
 
         // desenvolver e montar o listener dos botões aqui
-        customGridAdapter = new AdapterTecla(getActivity(), R.layout.adapter_tecla, gridArray);
+        customGridAdapter = new AdapterTecla(getActivity(), R.layout.adapter_tecla, gridTeclas);
 
         gridView.setAdapter(customGridAdapter);
 
@@ -44,159 +44,159 @@ public class FragmentConjuntoTeclas extends Fragment {
     private void setVisografemasNoGrupoAtual(int numeroGrupoDeVisografemas) {
         // Sem este IF, o array continua a ser populado por novas "teclas", mas
         // sem renderizar na tela.
-        if (gridArray.size() >= 0) {
-            gridArray.clear();
+        if (gridTeclas.size() >= 0) {
+            gridTeclas.clear();
         }
 
         switch (numeroGrupoDeVisografemas) {
 
             case Constantes.CONFIGURACAO_DE_DEDO:
-                gridArray.add(new Visografema("q"));
-                gridArray.add(new Visografema("w"));
-                gridArray.add(new Visografema("e"));
-                gridArray.add(new Visografema("r"));
-                gridArray.add(new Visografema("t"));
-                gridArray.add(new Visografema("y"));
-                gridArray.add(new Visografema("u"));
-                gridArray.add(new Visografema("i"));
-                gridArray.add(new Visografema("o"));
-                gridArray.add(new Visografema("p"));
-                gridArray.add(new Visografema("¹"));
-                gridArray.add(new Visografema("²"));
-                gridArray.add(new Visografema("a"));
-                gridArray.add(new Visografema("s"));
-                gridArray.add(new Visografema("d"));
-                gridArray.add(new Visografema("³"));
-                gridArray.add(new Visografema("£"));
-                gridArray.add(new Visografema("f"));
-                gridArray.add(new Visografema("g"));
-                gridArray.add(new Visografema("h"));
-                gridArray.add(new Visografema("¢"));
-                gridArray.add(new Visografema("¬"));
-                gridArray.add(new Visografema("j"));
-                gridArray.add(new Visografema("k"));
+                gridTeclas.add(new Visografema("q"));
+                gridTeclas.add(new Visografema("w"));
+                gridTeclas.add(new Visografema("e"));
+                gridTeclas.add(new Visografema("r"));
+                gridTeclas.add(new Visografema("t"));
+                gridTeclas.add(new Visografema("y"));
+                gridTeclas.add(new Visografema("u"));
+                gridTeclas.add(new Visografema("i"));
+                gridTeclas.add(new Visografema("o"));
+                gridTeclas.add(new Visografema("p"));
+                gridTeclas.add(new Visografema("¹"));
+                gridTeclas.add(new Visografema("²"));
+                gridTeclas.add(new Visografema("a"));
+                gridTeclas.add(new Visografema("s"));
+                gridTeclas.add(new Visografema("d"));
+                gridTeclas.add(new Visografema("³"));
+                gridTeclas.add(new Visografema("£"));
+                gridTeclas.add(new Visografema("f"));
+                gridTeclas.add(new Visografema("g"));
+                gridTeclas.add(new Visografema("h"));
+                gridTeclas.add(new Visografema("¢"));
+                gridTeclas.add(new Visografema("¬"));
+                gridTeclas.add(new Visografema("j"));
+                gridTeclas.add(new Visografema("k"));
                 break;
 
             case Constantes.ORIENTACAO_DE_PALMA:
-                gridArray.add(new Visografema("l"));
-                gridArray.add(new Visografema("ç"));
-                gridArray.add(new Visografema("z"));
-                gridArray.add(new Visografema("x"));
-                gridArray.add(new Visografema("c"));
-                gridArray.add(new Visografema("v"));
+                gridTeclas.add(new Visografema("l"));
+                gridTeclas.add(new Visografema("ç"));
+                gridTeclas.add(new Visografema("z"));
+                gridTeclas.add(new Visografema("x"));
+                gridTeclas.add(new Visografema("c"));
+                gridTeclas.add(new Visografema("v"));
                 break;
 
             case Constantes.PONTOS_DE_ARTICULACAO:
-                gridArray.add(new Visografema("Q"));
-                gridArray.add(new Visografema("W"));
-                gridArray.add(new Visografema("E"));
-                gridArray.add(new Visografema("R"));
-                gridArray.add(new Visografema("T"));
-                gridArray.add(new Visografema("Y"));
-                gridArray.add(new Visografema("U"));
-                gridArray.add(new Visografema("I"));
-                gridArray.add(new Visografema("O"));
-                gridArray.add(new Visografema("P"));
-                gridArray.add(new Visografema("A"));
-                gridArray.add(new Visografema("S"));
-                gridArray.add(new Visografema("D"));
-                gridArray.add(new Visografema("F"));
-                gridArray.add(new Visografema("G"));
-                gridArray.add(new Visografema("H"));
-                gridArray.add(new Visografema("J"));
-                gridArray.add(new Visografema("K"));
-                gridArray.add(new Visografema("L"));
-                gridArray.add(new Visografema("Ç"));
-                gridArray.add(new Visografema("\\"));
-                gridArray.add(new Visografema("Z"));
-                gridArray.add(new Visografema("X"));
-                gridArray.add(new Visografema("C"));
-                gridArray.add(new Visografema("V"));
-                gridArray.add(new Visografema("B"));
-                gridArray.add(new Visografema("N"));
-                gridArray.add(new Visografema("M"));
-                gridArray.add(new Visografema("@"));
-                gridArray.add(new Visografema("#"));
-                gridArray.add(new Visografema("$"));
-                gridArray.add(new Visografema("%"));
-                gridArray.add(new Visografema("&"));
-                gridArray.add(new Visografema("*"));
-                gridArray.add(new Visografema("_"));
+                gridTeclas.add(new Visografema("Q"));
+                gridTeclas.add(new Visografema("W"));
+                gridTeclas.add(new Visografema("E"));
+                gridTeclas.add(new Visografema("R"));
+                gridTeclas.add(new Visografema("T"));
+                gridTeclas.add(new Visografema("Y"));
+                gridTeclas.add(new Visografema("U"));
+                gridTeclas.add(new Visografema("I"));
+                gridTeclas.add(new Visografema("O"));
+                gridTeclas.add(new Visografema("P"));
+                gridTeclas.add(new Visografema("A"));
+                gridTeclas.add(new Visografema("S"));
+                gridTeclas.add(new Visografema("D"));
+                gridTeclas.add(new Visografema("F"));
+                gridTeclas.add(new Visografema("G"));
+                gridTeclas.add(new Visografema("H"));
+                gridTeclas.add(new Visografema("J"));
+                gridTeclas.add(new Visografema("K"));
+                gridTeclas.add(new Visografema("L"));
+                gridTeclas.add(new Visografema("Ç"));
+                gridTeclas.add(new Visografema("\\"));
+                gridTeclas.add(new Visografema("Z"));
+                gridTeclas.add(new Visografema("X"));
+                gridTeclas.add(new Visografema("C"));
+                gridTeclas.add(new Visografema("V"));
+                gridTeclas.add(new Visografema("B"));
+                gridTeclas.add(new Visografema("N"));
+                gridTeclas.add(new Visografema("M"));
+                gridTeclas.add(new Visografema("@"));
+                gridTeclas.add(new Visografema("#"));
+                gridTeclas.add(new Visografema("$"));
+                gridTeclas.add(new Visografema("%"));
+                gridTeclas.add(new Visografema("&"));
+                gridTeclas.add(new Visografema("*"));
+                gridTeclas.add(new Visografema("_"));
                 break;
 
             case Constantes.MOVIMENTOS:
-                gridArray.add(new Visografema("à"));
-                gridArray.add(new Visografema("á"));
-                gridArray.add(new Visografema("â"));
-                gridArray.add(new Visografema("ã"));
-                gridArray.add(new Visografema("ä"));
-                gridArray.add(new Visografema("è"));
-                gridArray.add(new Visografema("é"));
-                gridArray.add(new Visografema("ê"));
-                gridArray.add(new Visografema("ë"));
-                gridArray.add(new Visografema("ì"));
-                gridArray.add(new Visografema("í"));
-                gridArray.add(new Visografema("î"));
-                gridArray.add(new Visografema("ï"));
-                gridArray.add(new Visografema("ò"));
-                gridArray.add(new Visografema("ó"));
-                gridArray.add(new Visografema("ô"));
-                gridArray.add(new Visografema("õ"));
-                gridArray.add(new Visografema("ö"));
-                gridArray.add(new Visografema("ù"));
-                gridArray.add(new Visografema("ú"));
-                gridArray.add(new Visografema("û"));
-                gridArray.add(new Visografema("ü"));
-                gridArray.add(new Visografema("À"));
-                gridArray.add(new Visografema("Á"));
-                gridArray.add(new Visografema("Â"));
-                gridArray.add(new Visografema("Ã"));
-                gridArray.add(new Visografema("Ä"));
-                gridArray.add(new Visografema("È"));
-                gridArray.add(new Visografema("É"));
-                gridArray.add(new Visografema("Ê"));
-                gridArray.add(new Visografema("Ë"));
-                gridArray.add(new Visografema("Ì"));
-                gridArray.add(new Visografema("Í"));
-                gridArray.add(new Visografema("Î"));
-                gridArray.add(new Visografema("Ï"));
-                gridArray.add(new Visografema("Ò"));
-                gridArray.add(new Visografema("Ó"));
-                gridArray.add(new Visografema("Ô"));
-                gridArray.add(new Visografema("Õ"));
-                gridArray.add(new Visografema("Ö"));
-                gridArray.add(new Visografema("Ù"));
-                gridArray.add(new Visografema("Ú"));
-                gridArray.add(new Visografema("Û"));
-                gridArray.add(new Visografema("Ü"));
+                gridTeclas.add(new Visografema("à"));
+                gridTeclas.add(new Visografema("á"));
+                gridTeclas.add(new Visografema("â"));
+                gridTeclas.add(new Visografema("ã"));
+                gridTeclas.add(new Visografema("ä"));
+                gridTeclas.add(new Visografema("è"));
+                gridTeclas.add(new Visografema("é"));
+                gridTeclas.add(new Visografema("ê"));
+                gridTeclas.add(new Visografema("ë"));
+                gridTeclas.add(new Visografema("ì"));
+                gridTeclas.add(new Visografema("í"));
+                gridTeclas.add(new Visografema("î"));
+                gridTeclas.add(new Visografema("ï"));
+                gridTeclas.add(new Visografema("ò"));
+                gridTeclas.add(new Visografema("ó"));
+                gridTeclas.add(new Visografema("ô"));
+                gridTeclas.add(new Visografema("õ"));
+                gridTeclas.add(new Visografema("ö"));
+                gridTeclas.add(new Visografema("ù"));
+                gridTeclas.add(new Visografema("ú"));
+                gridTeclas.add(new Visografema("û"));
+                gridTeclas.add(new Visografema("ü"));
+                gridTeclas.add(new Visografema("À"));
+                gridTeclas.add(new Visografema("Á"));
+                gridTeclas.add(new Visografema("Â"));
+                gridTeclas.add(new Visografema("Ã"));
+                gridTeclas.add(new Visografema("Ä"));
+                gridTeclas.add(new Visografema("È"));
+                gridTeclas.add(new Visografema("É"));
+                gridTeclas.add(new Visografema("Ê"));
+                gridTeclas.add(new Visografema("Ë"));
+                gridTeclas.add(new Visografema("Ì"));
+                gridTeclas.add(new Visografema("Í"));
+                gridTeclas.add(new Visografema("Î"));
+                gridTeclas.add(new Visografema("Ï"));
+                gridTeclas.add(new Visografema("Ò"));
+                gridTeclas.add(new Visografema("Ó"));
+                gridTeclas.add(new Visografema("Ô"));
+                gridTeclas.add(new Visografema("Õ"));
+                gridTeclas.add(new Visografema("Ö"));
+                gridTeclas.add(new Visografema("Ù"));
+                gridTeclas.add(new Visografema("Ú"));
+                gridTeclas.add(new Visografema("Û"));
+                gridTeclas.add(new Visografema("Ü"));
                 break;
 
             case Constantes.PONTUACAO:
-                gridArray.add(new Visografema("/"));
-                gridArray.add(new Visografema("b"));
-                gridArray.add(new Visografema("-"));
-                gridArray.add(new Visografema(":"));
-                gridArray.add(new Visografema("."));
-                gridArray.add(new Visografema(","));
-                gridArray.add(new Visografema("?"));
-                gridArray.add(new Visografema("!"));
-                gridArray.add(new Visografema("Ý"));
-                gridArray.add(new Visografema("ý"));
-                gridArray.add(new Visografema("("));
-                gridArray.add(new Visografema(")"));
+                gridTeclas.add(new Visografema("/"));
+                gridTeclas.add(new Visografema("b"));
+                gridTeclas.add(new Visografema("-"));
+                gridTeclas.add(new Visografema(":"));
+                gridTeclas.add(new Visografema("."));
+                gridTeclas.add(new Visografema(","));
+                gridTeclas.add(new Visografema("?"));
+                gridTeclas.add(new Visografema("!"));
+                gridTeclas.add(new Visografema("Ý"));
+                gridTeclas.add(new Visografema("ý"));
+                gridTeclas.add(new Visografema("("));
+                gridTeclas.add(new Visografema(")"));
                 break;
 
             case Constantes.NUMEROS:
-                gridArray.add(new Visografema("0"));
-                gridArray.add(new Visografema("1"));
-                gridArray.add(new Visografema("2"));
-                gridArray.add(new Visografema("3"));
-                gridArray.add(new Visografema("4"));
-                gridArray.add(new Visografema("5"));
-                gridArray.add(new Visografema("6"));
-                gridArray.add(new Visografema("7"));
-                gridArray.add(new Visografema("8"));
-                gridArray.add(new Visografema("9"));
+                gridTeclas.add(new Visografema("0"));
+                gridTeclas.add(new Visografema("1"));
+                gridTeclas.add(new Visografema("2"));
+                gridTeclas.add(new Visografema("3"));
+                gridTeclas.add(new Visografema("4"));
+                gridTeclas.add(new Visografema("5"));
+                gridTeclas.add(new Visografema("6"));
+                gridTeclas.add(new Visografema("7"));
+                gridTeclas.add(new Visografema("8"));
+                gridTeclas.add(new Visografema("9"));
                 break;
 
         }
