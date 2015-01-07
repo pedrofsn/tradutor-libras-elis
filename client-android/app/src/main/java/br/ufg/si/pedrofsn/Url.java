260399e738b71ee4ce774b0e60d9dbdbc7e1f4bf
pedrofsn <pedrokra@gmail.com>
2015-01-07 13:03:01 -0200

    Teste de template

--- client-android/app/src/main/java/br/ufg/si/pedrofsn/Url.java
+++
@@ -1,8 +0,0 @@
-package br.ufg.si.pedrofsn;
-
-/**
- * Created by pedrofsn on 30/11/2014.
- */
-public class Url {
-    public static final String URL = "http://104.236.44.195:8080/api/termos/";
-}
--- server-node/app/models/termo.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Termo', {
-	ptbr : String,
-	elis : String
-});
