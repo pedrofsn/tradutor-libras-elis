a6c7e30391e8526a0c0d20ce9cb4b1de2073cde8
pedrofsn <pedrokra@gmail.com>
2015-03-20 15:23:11 -0300

    pequenas modificações = brincando um pouco

--- client-multi/app-abas/www/js/services.js
+++ client-multi/app-abas/www/js/services.js
@@ -6,29 +6,14 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
+    name: '@pedrofsn',
+    lastText: 'I\'m Android Developer',
     face: 'https://raw.githubusercontent.com/pedrofsn/pedrofsn/gh-pages/images/me.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    name: '@luatane',
+    lastText: 'Eu sei ELiS',
+    face: 'https://pbs.twimg.com/profile_images/527423002674487296/FJ6MF0lq.jpeg'
   }];
 
   return {
